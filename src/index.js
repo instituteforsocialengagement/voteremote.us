db9720f0abfe3cbad23b363ec238144614819cdc
Chris Snyder <christopher.snyder.m@gmail.com>
2018-09-18 18:00:26 -0400

    Google Analytics will now track each page view.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
-import ScrollToTop from './components/general-helpers/ScrollToTop';
+import AppSetupHelpers from './components/general-helpers/AppSetupHelpers';
 import registerServiceWorker from './registerServiceWorker';
 
 import App from './App';
 import 'semantic-ui/dist/semantic.min.css';
 
-import ReactGA from 'react-ga';
-ReactGA.initialize('UA-125429696-1');
-ReactGA.pageview(window.location.pathname + window.location.search);
-
 ReactDOM.render(
   <HashRouter>
-    <ScrollToTop>
+    <AppSetupHelpers>
       <App />
-    </ScrollToTop>
+    </AppSetupHelpers>
   </HashRouter>, 
   document.getElementById('vote-remote')
 );
