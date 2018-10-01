60ee3ce14cb0ada42c2c280dae5c34722ce93ac1
Chris Snyder <christopher.snyder.m@gmail.com>
2018-10-01 14:18:48 -0400

    Added Google Analytics external link tracker to footer link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import SocialMediaLinks from './SocialMediaLinks';
+import ReactGA from 'react-ga';
 
 import '../styles/Footer.css';
 
@@ -13,7 +14,12 @@ const Footer = () => (
             <Link className="link_no_decoration" to="/terms-and-privacy">Terms & Privacy</Link>
         </div>
         <SocialMediaLinks className="social_box cr"/>
-        <p className="footer_box">Vote Remote is a project of the Institute for Social Engagement, a 501(c)(3) organization.</p>
+        <p>Vote Remote is a project of the <ReactGA.OutboundLink
+            eventLabel="Footer link to Think Informed"
+            to="http://www.thinkinformed.org"
+            target="_blank">
+            Institute for Social Engagement
+            </ReactGA.OutboundLink>, a 501(c)(3) organization.</p>
     </div>
 );
 
