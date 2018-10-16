dfe56da2e3fa4b963e3b228f878aef8966ac9359
Chris Snyder <christopher.snyder.m@gmail.com>
2018-10-16 16:08:43 -0400

    VoteLOL Easter egg installation on footer bar + disclaimer page

---
+++ src/components/VoteLolLinkPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../styles/AboutPage.css';
+
+const VoteLolLinkPage = () => (
+    <div>
+        <div className="about_content_container">
+            <div className="about_top_container">
+                <div className="about_top_container_text">
+                    <p>You are leaving Vote Remote. The website you are going to is not affiliated with the Institute for Social Engagement.</p>
+                    <p>Click "Continue" below to proceed.</p>
+                    <h2><a class="bold" href="http://www.vote.lol">Continue</a></h2>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default VoteLolLinkPage;
