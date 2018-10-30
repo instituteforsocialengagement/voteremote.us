3ec429685d40b1e17b5f39c66d47d0dae7cf8ec9
Chris Snyder <christopher.snyder.m@gmail.com>
2018-10-30 17:10:16 -0400

    Made update to VoteLOL Easter egg disclaimer page

--- src/components/VoteLolLinkPage.js
+++ src/components/VoteLolLinkPage.js
@@ -6,7 +6,7 @@ const VoteLolLinkPage = () => (
         <div className="about_content_container">
             <div className="about_top_container">
                 <div className="about_top_container_text">
-                    <p>You are leaving Vote Remote. The website you are going to is not affiliated with the Institute for Social Engagement.</p>
+                    <p>You are leaving Vote Remote. The website you are going to, VoteLOL, is not affiliated with the Institute for Social Engagement. VoteLOL is a game developed by MIT students to encourage voter turnout through gamification.</p>
                     <p>Click "Continue" below to proceed.</p>
                     <h2><a class="bold" href="http://www.vote.lol">Continue</a></h2>
                 </div>
